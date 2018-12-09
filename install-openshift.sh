edbfa640a6127b6817cf201faf6d4876ce2044c1
Marcelo Sales <marcelo.d.sales@gmail.com>
2018-12-09 00:52:00 -0200

    update sh

--- install-openshift.sh
+++ install-openshift.sh
@@ -1,49 +1,103 @@
 #!/bin/bash
 
-## see: https://www.youtube.com/watch?v=-OOnGK-XeVY
+## see: https://youtu.be/aqXSbDZggK4
 
-export DOMAIN=${DOMAIN:="$(curl ipinfo.io/ip).nip.io"}
+## Default variables to use
+export INTERACTIVE=${INTERACTIVE:="true"}
+export PVS=${INTERACTIVE:="true"}
+#export DOMAIN=${DOMAIN:="$(curl -s ipinfo.io/ip).nip.io"}
+export DOMAIN=${DOMAIN:="$(hostname -I | awk '{print $1}').nip.io"}
 export USERNAME=${USERNAME:="$(whoami)"}
 export PASSWORD=${PASSWORD:=password}
-export VERSION=${VERSION:="v3.7.1"}
-
+export VERSION=${VERSION:="3.11"}
 export SCRIPT_REPO=${SCRIPT_REPO:="https://raw.githubusercontent.com/gshipley/installcentos/master"}
+export IP=${IP:="$(ip route get 8.8.8.8 | awk '{print $NF; exit}')"}
+export API_PORT=${API_PORT:="8443"}
 
-export IP="$(ip route get 8.8.8.8 | awk '{print $NF; exit}')"
+## Make the script interactive to set the variables
+if [ "$INTERACTIVE" = "true" ]; then
+	read -rp "Domain to use: ($DOMAIN): " choice;
+	if [ "$choice" != "" ] ; then
+		export DOMAIN="$choice";
+	fi
+
+	read -rp "Username: ($USERNAME): " choice;
+	if [ "$choice" != "" ] ; then
+		export USERNAME="$choice";
+	fi
+
+	read -rp "Password: ($PASSWORD): " choice;
+	if [ "$choice" != "" ] ; then
+		export PASSWORD="$choice";
+	fi
+
+	read -rp "OpenShift Version: ($VERSION): " choice;
+	if [ "$choice" != "" ] ; then
+		export VERSION="$choice";
+	fi
+	read -rp "IP: ($IP): " choice;
+	if [ "$choice" != "" ] ; then
+		export IP="$choice";
+	fi
+
+	read -rp "API Port: ($API_PORT): " choice;
+	if [ "$choice" != "" ] ; then
+		export API_PORT="$choice";
+	fi
+
+	echo
+
+fi
 
 echo "******"
 echo "* Your domain is $DOMAIN "
+echo "* Your IP is $IP "
 echo "* Your username is $USERNAME "
 echo "* Your password is $PASSWORD "
 echo "* OpenShift version: $VERSION "
 echo "******"
 
-yum install -y epel-release
+# install updates
+yum update -y
 
-yum install -y git wget zile nano net-tools docker \
-python-cryptography pyOpenSSL.x86_64 python2-pip \
-openssl-devel python-devel httpd-tools NetworkManager python-passlib \
-java-1.8.0-openjdk-headless "@Development Tools"
+# install the following base packages
+yum install -y  wget git zile nano net-tools docker-1.13.1\
+				bind-utils iptables-services \
+				bridge-utils bash-completion \
+				kexec-tools sos psacct openssl-devel \
+				httpd-tools NetworkManager \
+				python-cryptography python2-pip python-devel  python-passlib \
+				java-1.8.0-openjdk-headless "@Development Tools"
 
-systemctl | grep "NetworkManager.*running" 
+#install epel
+yum -y install epel-release
+
+# Disable the EPEL repository globally so that is not accidentally used during later steps of the installation
+sed -i -e "s/^enabled=1/enabled=0/" /etc/yum.repos.d/epel.repo
+
+systemctl | grep "NetworkManager.*running"
 if [ $? -eq 1 ]; then
 	systemctl start NetworkManager
 	systemctl enable NetworkManager
 fi
 
-which ansible || pip install -Iv ansible
+# install the packages for Ansible
+yum -y --enablerepo=epel install pyOpenSSL
+
+curl -o ansible.rpm https://releases.ansible.com/ansible/rpm/release/epel-7-x86_64/ansible-2.6.5-1.el7.ans.noarch.rpm
+yum -y --enablerepo=epel install ansible.rpm
 
 [ ! -d openshift-ansible ] && git clone https://github.com/openshift/openshift-ansible.git
 
-cd openshift-ansible && git fetch && git checkout release-3.7 && cd ..
+cd openshift-ansible && git fetch && git checkout release-${VERSION} && cd ..
 
 cat <<EOD > /etc/hosts
-127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4 
+127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4
 ::1         localhost localhost.localdomain localhost6 localhost6.localdomain6
-${IP}		$(hostname) console console.${DOMAIN} 
+${IP}		$(hostname) console console.${DOMAIN} $(hostname).${DOMAIN}
 EOD
 
-if [ -z $DISK ]; then 
+if [ -z $DISK ]; then
 	echo "Not setting the Docker storage."
 else
 	cp /etc/sysconfig/docker-storage-setup /etc/sysconfig/docker-storage-setup.bk
@@ -78,27 +132,62 @@ if [ "$memory" -lt "4194304" ]; then
 	export METRICS="False"
 fi
 
-if [ "$memory" -lt "8388608" ]; then
+if [ "$memory" -lt "16777216" ]; then
 	export LOGGING="False"
 fi
 
 curl -o inventory.download $SCRIPT_REPO/inventory.ini
 envsubst < inventory.download > inventory.ini
-ansible-playbook -i inventory.ini openshift-ansible/playbooks/byo/config.yml
+
+# add proxy in inventory.ini if proxy variables are set
+if [ ! -z "${HTTPS_PROXY:-${https_proxy:-${HTTP_PROXY:-${http_proxy}}}}" ]; then
+	echo >> inventory.ini
+	echo "openshift_http_proxy=\"${HTTP_PROXY:-${http_proxy:-${HTTPS_PROXY:-${https_proxy}}}}\"" >> inventory.ini
+	echo "openshift_https_proxy=\"${HTTPS_PROXY:-${https_proxy:-${HTTP_PROXY:-${http_proxy}}}}\"" >> inventory.ini
+	if [ ! -z "${NO_PROXY:-${no_proxy}}" ]; then
+		__no_proxy="${NO_PROXY:-${no_proxy}},${IP},.${DOMAIN}"
+	else
+		__no_proxy="${IP},.${DOMAIN}"
+	fi
+	echo "openshift_no_proxy=\"${__no_proxy}\"" >> inventory.ini
+fi
+
+mkdir -p /etc/origin/master/
+touch /etc/origin/master/htpasswd
+
+ansible-playbook -i inventory.ini openshift-ansible/playbooks/prerequisites.yml
+ansible-playbook -i inventory.ini openshift-ansible/playbooks/deploy_cluster.yml
 
 htpasswd -b /etc/origin/master/htpasswd ${USERNAME} ${PASSWORD}
 oc adm policy add-cluster-role-to-user cluster-admin ${USERNAME}
 
-systemctl restart origin-master-api
+if [ "$PVS" = "true" ]; then
+
+	curl -o vol.yaml $SCRIPT_REPO/vol.yaml
+
+	for i in `seq 1 200`;
+	do
+		DIRNAME="vol$i"
+		mkdir -p /mnt/data/$DIRNAME
+		chcon -Rt svirt_sandbox_file_t /mnt/data/$DIRNAME
+		chmod 777 /mnt/data/$DIRNAME
+
+		sed "s/name: vol/name: vol$i/g" vol.yaml > oc_vol.yaml
+		sed -i "s/path: \/mnt\/data\/vol/path: \/mnt\/data\/vol$i/g" oc_vol.yaml
+		oc create -f oc_vol.yaml
+		echo "created volume $i"
+	done
+	rm oc_vol.yaml
+fi
 
 echo "******"
-echo "* Your conosle is https://console.$DOMAIN:8443"
+echo "* Your console is https://console.$DOMAIN:$API_PORT"
 echo "* Your username is $USERNAME "
 echo "* Your password is $PASSWORD "
 echo "*"
 echo "* Login using:"
 echo "*"
-echo "$ oc login -u ${USERNAME} -p ${PASSWORD} https://console.$DOMAIN:8443/"
+echo "$ oc login -u ${USERNAME} -p ${PASSWORD} https://console.$DOMAIN:$API_PORT/"
 echo "******"
 
-oc login -u ${USERNAME} -p ${PASSWORD} https://console.$DOMAIN:8443/
+oc login -u ${USERNAME} -p ${PASSWORD} https://console.$DOMAIN:$API_PORT/
